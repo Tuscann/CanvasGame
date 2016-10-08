27edcb20975f9270d41be3b1a443bfe7358bac2a
Tuscann <fbinnzhivko@gmail.com>
2016-10-08 16:25:14 +0300

    a
    a

--- javacript/index.js
+++ javacript/index.js
@@ -1,4 +1,8 @@
-function solve (input) {
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+var imageObj = new Image();
 
-
-}
+imageObj.onload = function() {
+    context.drawImage(imageObj, 69, 50);
+};
+imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
