3bbb506f44c7049314ecd43297824fe4707df277
achobanov <alexchobanov@gmail.com>
2016-10-09 22:38:36 +0300

    Backgammon v0.1 - Board grid with possitions. Pieces, assigned on board possition. Starting pieces setup. Possible moves, based on selected piece and dice rolls (all appropriate checks included). NOT TESTED - lack of working canvas

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,20 @@
 "use strict";
 
+let winner = false;
+
 function gameLoop() {
     initCanvas();
-    renderBoard()
+    renderBoard();
+    setupGame();
 
+    if (winner) {
+        clearInterval(intervalId);
+    }
+    var FPS = 30;
+    let intervalId = setInterval(function() {
+        update();
+        render();
+    }, 1000/FPS);
 }
 
 gameLoop();
--- scripts/resources.js
+++ scripts/resources.js
@@ -2,41 +2,149 @@
 let canvas;
 let ctx;
 let pieceCounter = 0;
+let isPieceSelected = false;
+let dice1, dice2;
+let availablePositionsToPutPieceIn;
 
-let grid = {
-    position1: {x: 724, y:40, status: 'free'},
-    position2: {x: 669, y:40, status: 'free'},
-    position3: {x: 614, y:40, status: 'free'},
-    position4: {x: 559, y:40, status: 'free'},
-    position5: {x: 504, y:40, status: 'free'},
-    position6: {x: 449, y:40, status: 'free'},
-    position7: {x: 339, y:40, status: 'free'},
-    position8: {x: 284, y:40, status: 'free'},
-    position9: {x: 229, y:40, status: 'free'},
-    position10: {x: 174, y:40, status: 'free'},
-    position11: {x: 119, y:40, status: 'free'},
-    position12: {x: 64, y:40, status: 'free'},
-    position13: {x: 64, y:505, status: 'free'},
-    position14: {x: 119, y:505, status: 'free'},
-    position15: {x: 174, y:505, status: 'free'},
-    position16: {x: 229, y:505, status: 'free'},
-    position17: {x: 284, y:505, status: 'free'},
-    position18: {x: 339, y:505, status: 'free'},
-    position19: {x: 449, y:505, status: 'free'},
-    position20: {x: 504, y:505, status: 'free'},
-    position21: {x: 559, y:505, status: 'free'},
-    position22: {x: 614, y:505, status: 'free'},
-    position23: {x: 669, y:505, status: 'free'},
-    position24: {x: 724, y:505, status: 'free'},
-};
 
-function pieceBuilder(color) {
+function update() {
+    if (isPieceSelected) calculatePossibleMoves(isPieceSelected); //TODO: calculate possible moves for 'isPieceSelected' ( holds the id of the selected piece ).
+}
+
+function render() {
+    ctx.clearRect(0, 0, 840, 600);
+    renderBoard();
+    renderPieces();
+    rencerDices();
+}
+
+function calculatePossibleMoves(selectedPieceId) { // returns list of areas for input collision. TODO: remember to clear list on 'piece.isSelected' listener, when piece is put down.
+    // _dev - assignment of dices for tests.
+    // [dice1, dice2] = [2, 4];
+
+    availablePositionsToPutPieceIn = [];
+    let selectedPiece;
+    let selectedPiecePosition;
+
+    // This loop is probably not necessary. I might just assign the piece object itself as argument.
+    loop1:
+        for (let position of board) {
+            for (let piece of position) {
+                if (piece.id === selectedPieceId) {
+                    selectedPiece = piece;
+                    selectedPiecePosition = position;
+                    break loop1; // Breaks both loops when finds matching id
+                }
+            }
+        }
+
+    let opponent = (selectedPiece.color === 'white')? 'black': 'white';
+
+    // List of available moves, according to dice and selectedPiece position.
+    let moves = [
+        selectedPiecePosition + dice1,
+        selectedPiecePosition + dice2,
+        selectedPiecePosition + dice1 + dice2
+    ];
+
+    // Array of booleans, checking if the target position is free, occupied by same team pieces, occupied by 1 opponent team piece or occupied by more opponent pieces.
+    let possibleMoves = [
+        board[moves[0]].occupiedBy === 'none' ||
+        board[moves[0]].occupiedBy === selectedPiece.color ||
+        board[moves[0]].occupiedBy === opponent && board[moves[0]].piecesOn.length === 1,
+
+        board[moves[1]].occupiedBy === 'none' ||
+        board[moves[1]].occupiedBy === selectedPiece.color ||
+        board[moves[1]].occupiedBy === opponent && board[moves[1]].piecesOn.length === 1,
+    ];
+    possibleMoves.push(
+        (possibleMoves[0] || possibleMoves[1]) &&
+        (board[moves[2]].occupiedBy === 'none' ||
+        board[moves[2]].occupiedBy === selectedPiece.color ||
+        board[moves[2]].occupiedBy === opponent && board[moves[2]].piecesOn.length === 1)
+    );
+
+    // Adding [0-3] possible positions to list, for dropSelectedPiece listener.
+    for (let i = 0; i < 3; i++) {
+        if (possibleMoves[i]) {
+            availablePositionsToPutPieceIn.push({
+                x: {start: board[moves[i]].x.start, end: board[moves[i]].x.end},
+                y: {start: board[moves[i]].y.start, end: board[moves[i]].y.end}
+            })
+        }
+    }
+}
+
+function setupGame() {
+    initPiece('white', 0);
+    initPiece('white', 0);
+
+    initPiece('white', 11);
+    initPiece('white', 11);
+    initPiece('white', 11);
+    initPiece('white', 11);
+    initPiece('white', 11);
+
+    initPiece('white', 16);
+    initPiece('white', 16);
+    initPiece('white', 16);
+
+    initPiece('white', 18);
+    initPiece('white', 18);
+    initPiece('white', 18);
+    initPiece('white', 18);
+    initPiece('white', 18);
+
+    initPiece('black', 23);
+    initPiece('black', 23);
+
+    initPiece('black', 12);
+    initPiece('black', 12);
+    initPiece('black', 12);
+    initPiece('black', 12);
+    initPiece('black', 12);
+
+    initPiece('black', 7);
+    initPiece('black', 7);
+    initPiece('black', 7);
+
+    initPiece('black', 5);
+    initPiece('black', 5);
+    initPiece('black', 5);
+    initPiece('black', 5);
+    initPiece('black', 5);
+}
+
+function initPiece(color, position) {
+    let piece = pieceBuilder(color, position);
+    board[position].piecesOn.push(piece);
+}
+
+function pieceBuilder(color, position) {
     pieceCounter++;
     return {
         id: color + pieceCounter,
         color: color,
+        y: function() { // Positions up to 5 pieces on the position. TODO: implement indication in case of more then 5 pieces.
+            let piecesOnPosition = board[position].piecesOn.length;
+            if (piecesOnPosition >= 5) {
+                return - 800;
+            } else {
+                if (position < 12) {
+                    return 40 + piecesOnPosition * 55;
+                } else {
+                    return 560 - piecesOnPosition * 55;
+                }
+            }
+        },
+        side:55,
+        selectedSide:60,
+        img: new Image({
+            src: './resources/piece.png'
+        }),
         inPlay: true,
-        score: false,
+        isSelected: false,
+        score: false
     }
 }
 
@@ -55,6 +163,21 @@ function renderBoard() {
     board.onload = function() {
         ctx.drawImage(board, 0, 0);
     };
-
 }
- 
+
+function renderPieces() {
+    for (let position of board) {
+        for (let piece of position) {
+            let image = piece.img,
+                x = piece.position.x,
+                y = piece.position.y,
+                side = piece.side;
+            if (piece.selected) {
+                side = piece.selectedSide;
+            }
+
+            ctx.drawImage(image, x, y, side, side);  // Possibly not working - expecting buggy behaviour, because of 'onload' crap.
+        }
+    }
+}
+
